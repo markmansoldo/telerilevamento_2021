f7a0f96e718d16e6693faacfe94d403055974abc
markmansoldo <80513535+markmansoldo@users.noreply.github.com>
2021-03-26 10:53:30 +0100

    Update R_code_remote_sensing_first.r

--- R_code_remote_sensing_first.r
+++ R_code_remote_sensing_first.r
@@ -18,3 +18,87 @@ plot(p224r63_2011, col=cl)
 #colour change exercise
 cl2 <- colorRampPalette(c("dark blue","medium blue","light blue")) (100)
 plot(p224r63_2011, col=cl2)
+
+cls <- colorRampPalette(c("red","pink","orange","purple")) (200)
+plot(p224r63_2011, col=cls)
+
+dev.off()
+plot (p224r63_2011, col=cl)
+plot(p224r63_2011$B1_sre)
+plot(p224r63_2011$B2_sre)
+
+par(mfrow=c(1,2))
+plot(p224r63_2011$B1_sre)
+plot(p224r63_2011$B2_sre)
+
+# 2 row, 1 columns
+par(mfrow=c(2,1))
+plot(p224r63_2011$B1_sre)
+plot(p224r63_2011$B2_sre)
+
+# plot the first four bands of Landsat
+par(mfrow=c(4,1))
+plot(p224r63_2011$B1_sre)
+plot(p224r63_2011$B2_sre)
+plot(p224r63_2011$B3_sre)
+plot(p224r63_2011$B4_sre)
+
+# a quadrat of bands...:
+par(mfrow=c(2,2))
+plot(p224r63_2011$B1_sre)
+plot(p224r63_2011$B2_sre)
+plot(p224r63_2011$B3_sre)
+plot(p224r63_2011$B4_sre)
+
+par(mfrow=c(2,2))
+ 
+clb <- colorRampPalette(c("dark blue","blue","light blue")) (100)
+plot(p224r63_2011$B1_sre, col=clb)
+
+clg <- colorRampPalette(c("dark green","green","light green")) (100)
+plot(p224r63_2011$B2_sre, col=clg)
+
+clr <- colorRampPalette(c("dark red","red","pink")) (100)
+plot(p224r63_2011$B3_sre, col=clr)
+
+clnir <- colorRampPalette(c("red","orange","yellow")) (100)
+plot(p224r63_2011$B4_sre, col=clnir)
+
+# Bande Landsat
+# B1: blu
+# B2: verde
+# B3: rosso
+# B4: infrarosso vicino
+# B5: infrarosso medio
+# B6: infrarosso termico
+# B7: infrarosso medio
+
+plotRGB(p224r63_2011, r=3, g=2, b=1, stretch="Lin") #molto scuro
+
+plotRGB(p224r63_2011, r=4, g=3, b=2, stretch="Lin") #molto rosso
+
+plotRGB(p224r63_2011, r=3, g=4, b=2, stretch="Lin") #molto verde
+
+plotRGB(p224r63_2011, r=3, g=2, b=4, stretch="Lin") #molto blu
+
+# Exercise: mount a 2x2 multiframe
+
+pdf("il_mio_primo_pdf_con_R.pdf")
+par(mfrow=c(2,2))
+plotRGB(p224r63_2011, r=3, g=2, b=1, stretch="Lin")
+plotRGB(p224r63_2011, r=4, g=3, b=2, stretch="Lin")
+plotRGB(p224r63_2011, r=3, g=4, b=2, stretch="Lin")
+plotRGB(p224r63_2011, r=3, g=2, b=4, stretch="Lin")
+dev.off()
+
+plotRGB(p224r63_2011, r=3, g=4, b=2, stretch="hist") #verde con magenta
+
+ # par natural colours, false colours, and false colours with histogram stretching
+par(mfrow=c(3,1))
+plotRGB(p224r63_2011, r=3, g=2, b=1, stretch="Lin")
+plotRGB(p224r63_2011, r=3, g=4, b=2, stretch="Lin")
+plotRGB(p224r63_2011, r=3, g=4, b=2, stretch="hist")
+
+install.packages("RStoolbox")
+library(RStoolbox)
+
