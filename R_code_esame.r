f267934d19b3ce9b4b7d7b8c592f19db9025ed20
markmansoldo <80513535+markmansoldo@users.noreply.github.com>
2021-08-04 00:20:38 +0200

    Update R_code_esame.r

--- R_code_esame.r
+++ R_code_esame.r
@@ -16,18 +16,26 @@ setwd("C:/exam/")
 # __________Importing images__________________________________________________________________________________________________________________________________________________
 
 # To list all the files together, with the common pattern "cod" in the file name:
-list2021 <- list.files(pattern="cod_2021")
-list2021
+list2020 <- list.files(pattern="cod_2020")
+list2020
 
 # To have each file as a raster brick, with each image containing three layers:
-import2021 <- lapply(list2021,brick)
-import2021
+import2020 <- lapply(list2020,brick)
+import2020
 
 # To stack the bands of interest for basic colour analysis and NDWI analysis: (Band 1: Blue, Band 2: Green, Band 3: Red, Band 4: NIR, Band 5: SWIR-1)
-cod_2021 <- stack(import2021)
-cod_2021
+cod_2020 <- stack(import2020)
+cod_2020
 
-# This process was done for all four years to be analyzed: 2006, 2011, 2016, 2021
+# This process was done for all four years to be analyzed: 2014, 2016, 2018, 2020
+
+# __________2018______________________________________________________________________________________________________________________________________________________________
+list2018 <- list.files(pattern="cod_2018")
+list2018
+import2018 <- lapply(list2018,brick)
+import2018
+cod_2018 <- stack(import2018)
+cod_2018
 
 # __________2016______________________________________________________________________________________________________________________________________________________________
 list2016 <- list.files(pattern="cod_2016")
@@ -37,26 +45,18 @@ import2016
 cod_2016 <- stack(import2016)
 cod_2016
 
-# __________2011______________________________________________________________________________________________________________________________________________________________
-list2011 <- list.files(pattern="cod_2011")
-list2011
-import2011 <- lapply(list2011,brick)
-import2011
-cod_2011 <- stack(import2011)
-cod_2011
-
-# __________2006______________________________________________________________________________________________________________________________________________________________
-list2006 <- list.files(pattern="cod_2006")
-list2006
-import2006 <- lapply(list2006,brick)
-import2006
-cod_2006 <- stack(import2006)
-cod_2006
+# __________2014______________________________________________________________________________________________________________________________________________________________
+list2014 <- list.files(pattern="cod_2014")
+list2014
+import2014 <- lapply(list2014,brick)
+import2014
+cod_2014 <- stack(import2014)
+cod_2014
 
 # __________Selecting study area using drawExtent to crop the images__________________________________________________________________________________________________________
 
 # Upload one image to understand the dimensions to be cropped:
-cropbackground <- raster("cod_2006_B1.tif")
+cropbackground <- raster("cod_2020_B1.tif")
 plot(cropbackground)
 
 # Use the drawExtent function to select the area to crop on the image:
@@ -73,8 +73,5 @@ new_extent <- extent(412065, 425655, 4596555, 4614825)
 class(new_extent)
 
 # To check that the "extent" works and create a new cropped area for another image:
-cropcod_2006 <- crop(x = cod_2006, y = new_extent)
+cropcod_2018 <- crop(x = cod_2018, y = new_extent)
 
-# __________A new crop extent is required for each year due to slight variations in satellite flight path_____________________________________________________________________
-
-# __________drawExtent 2016___________________________________________________________________________________________________________________________________________________
