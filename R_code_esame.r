4c9df080a35013d362b23210f50d1f6c4db5e0c2
markmansoldo <80513535+markmansoldo@users.noreply.github.com>
2021-08-04 02:11:11 +0200

    Update R_code_esame.r

--- R_code_esame.r
+++ R_code_esame.r
@@ -92,7 +92,7 @@ plot(cropcod_2014)
 # __________Colour palettes___________________________________________________________________________________________________________________________________________________
 
 # Colour palettes I created:
-mono <- colorRampPalette(c("black","dark grey","light grey"))(100)
+mono <- colorRampPalette(c("black","light grey","white"))(100)
 greek <- colorRampPalette(c("dark blue","blue","white"))(100)
 blbl <- colorRampPalette(c("black","blue","white"))(100)
 changer <- colorRampPalette(c("steelblue3","black","yellow"))(100)
@@ -100,10 +100,88 @@ fire <- colorRampPalette(c("white","orange3","purple"))(100)
 
 # __________Experimenting with RGB plots______________________________________________________________________________________________________________________________________
 
-# To plot all of them together
+# Landsat 8 spectral bands:
+# Band 1 = Coastal / Aerosol
+# Band 2 = Blue
+# Band 3 = Green
+# Band 4 = Red
+# Band 5 = NIR
+# Band 6 = SWIR-1
+# Band 7 = SWIR-2
+# Band 9 = Cirrus
+
+# To plot all of them together:
 par(mfrow=c(2,2))
 plotRGB(cropcod_2014, 4, 3, 2, stretch="Lin")
 plotRGB(cropcod_2018, 4, 3, 2, stretch="Lin")
 plotRGB(cropcod_2016, 4, 3, 2, stretch="Lin")
 plotRGB(cropcod_2020, 4, 3, 2, stretch="Lin")
 
+# Emphasises coastal sands with bright white sand:
+par(mfrow=c(2,2))
+plotRGB(cropcod_2014, 1, 2, 3, stretch="Lin")
+plotRGB(cropcod_2018, 1, 2, 3, stretch="Lin")
+plotRGB(cropcod_2016, 1, 2, 3, stretch="Lin")
+plotRGB(cropcod_2020, 1, 2, 3, stretch="Lin")
+
+# Emphasises vegetation and terrain:
+par(mfrow=c(2,2))
+plotRGB(cropcod_2014, 5, 6, 7, stretch="Lin")
+plotRGB(cropcod_2018, 5, 6, 7, stretch="Lin")
+plotRGB(cropcod_2016, 5, 6, 7, stretch="Lin")
+plotRGB(cropcod_2020, 5, 6, 7, stretch="Lin")
+
+# Emphasises movement of submerged sediment:
+par(mfrow=c(2,2))
+plotRGB(cropcod_2014, 5, 6, 1, stretch="Lin")
+plotRGB(cropcod_2018, 5, 6, 1, stretch="Lin")
+plotRGB(cropcod_2016, 5, 6, 1, stretch="Lin")
+plotRGB(cropcod_2020, 5, 6, 1, stretch="Lin")
+
+# __________Experimenting with different bands within the bricks to visualize geomorphology___________________________________________________________________________________
+
+# Coastal geomorphology with first band (Coastal/Ultra-blue) of first and last year:
+par(mfrow=c(1,2))
+plot(cropcod_2014$cod_2014_B1, col=mono)
+plot(cropcod_2020$cod_2020_B1, col=mono)
+# Highlights sand bar breaks
+
+# __________Calculating the degree of change in geomorphology between 1993 and 2018___________________________________________________________________________________________
+
+# Subtracting the first year from the last year to find the difference:
+codchange <- cropcod_2020 - cropcod_2014
+plot(codchange, col=changer)
+# Bands 4-7 show very high contrast and illustrate well the degree of change
+
+# Investigating the degree of change shown by band 7 of each year, as it shows good contrast in active sediment zones:
+codchangeB7 <- cropcod_2020$cod_2020_B7 - cropcod_2014$cod_2014_B7
+
+# Levelplot, using previously created colour palette, to analyse the degree of change in band 7 (SWIR-2):
+levelplot(codchangeB7, col.regions=changer)
+
+# __________Principal Components Analysis (PCA)_______________________________________________________________________________________________________________________________
+
+# Aggregate the raster cells and look at the dimensions to see how many pixels there are per band:
+# Resampling by a factor of 10:
+
+# To conduct a PCA:
+
+# __________Unsupervised classification_______________________________________________________________________________________________________________________________________
+
+# To classify the brick into 2 classes:
+class2014 <- unsuperClass(cropcod_2014, nClasses=2)
+plot(class2014$map)
+
+class2020 <- unsuperClass(cropcod_2020, nClasses=2)
+plot(class2020$map)
+
+# __________Normalized Difference Water Index (NDWI)__________________________________________________________________________________________________________________________
+
+NDWI2014 <- (cropcod_2014$cod_2014_B3 - cropcod_2014$cod_2014_B5)/(cropcod_2014$cod_2014_B3 + cropcod_2014$cod_2014_B5)
+NDWI2020 <- (cropcod_2020$cod_2020_B3 - cropcod_2020$cod_2020_B5)/(cropcod_2020$cod_2020_B3 + cropcod_2020$cod_2020_B5)
+
+NDWIdiff <- NDWI2020-NDWI2014
+
+plot(NDWIdiff, col=changer)
+
+# ____________________________________________________________________________________________________________________________________________________________________________
