b9d67b3ebe00225d136908d4ec0f80f46677a0e2
markmansoldo <80513535+markmansoldo@users.noreply.github.com>
2021-08-06 14:46:10 +0200

    Update R_code_exam.r

--- R_code_exam.r
+++ R_code_exam.r
@@ -618,14 +618,14 @@ plot(NDWIa_monomoy_2016, col=mono)
 plot(NDWIa_monomoy_2018, col=mono)
 plot(NDWIa_monomoy_2020, col=mono)
 
-# Remove all values greater than 0 (remove water):
-NDWIa_monomoy_2014land <- reclassify(NDWIa_monomoy_2014, cbind(0, 1, NA))
-NDWIa_monomoy_2016land <- reclassify(NDWIa_monomoy_2016, cbind(0, 1, NA))
-NDWIa_monomoy_2018land <- reclassify(NDWIa_monomoy_2018, cbind(0, 1, NA))
-NDWIa_monomoy_2020land <- reclassify(NDWIa_monomoy_2020, cbind(0, 1, NA))
+# Remove all values between -0.1 and 1  to remove water and any tidal sands:
+NDWIa_monomoy_2014land <- reclassify(NDWIa_monomoy_2014, cbind(-0.1, 1, NA))
+NDWIa_monomoy_2016land <- reclassify(NDWIa_monomoy_2016, cbind(-0.1, 1, NA))
+NDWIa_monomoy_2018land <- reclassify(NDWIa_monomoy_2018, cbind(-0.1, 1, NA))
+NDWIa_monomoy_2020land <- reclassify(NDWIa_monomoy_2020, cbind(-0.1, 1, NA))
 
 # Showing only exposed land:
-# Brightest colour indicates tidal bars
+# Brightest colour indicates bare sand
 # Darkest colour indicates relatively dense vegetation
 par(mfrow=c(1,4))
 plot(NDWIa_monomoy_2014land, col=mono)
@@ -635,90 +635,59 @@ plot(NDWIa_monomoy_2020land, col=mono)
 
 # __________Unsupervised classification of Monomoy Nature Reserve_____________________________________________________________________________________________________________
 
-# 4 classes:
-# Tidal sand bars, Bare sand, Dune vegetation, Salt marsh #############################################################################CHECK
-classmonomoy_2014_4 <- unsuperClass(NDWIa_monomoy_2014land, nClasses=3)
-classmonomoy_2016_4 <- unsuperClass(NDWIa_monomoy_2016land, nClasses=3)
-classmonomoy_2018_4 <- unsuperClass(NDWIa_monomoy_2018land, nClasses=3)
-classmonomoy_2020_4 <- unsuperClass(NDWIa_monomoy_2020land, nClasses=3)
+# 3 classes:
+# Bare sand, Dune vegetation, Salt marsh
+classmonomoy_2014_3 <- unsuperClass(NDWIa_monomoy_2014land, nClasses=3)
+classmonomoy_2016_3 <- unsuperClass(NDWIa_monomoy_2016land, nClasses=3)
+classmonomoy_2018_3 <- unsuperClass(NDWIa_monomoy_2018land, nClasses=3)
+classmonomoy_2020_3 <- unsuperClass(NDWIa_monomoy_2020land, nClasses=3)
 
 par(mfrow=c(1,4))
-plot(classmonomoy_2014_4$map, col=changer)
-plot(classmonomoy_2016_4$map, col=changer)
-plot(classmonomoy_2018_4$map, col=changer)
-plot(classmonomoy_2020_4$map, col=changer)
+plot(classmonomoy_2014_3$map, col=changer)
+plot(classmonomoy_2016_3$map, col=changer)
+plot(classmonomoy_2018_3$map, col=changer)
+plot(classmonomoy_2020_3$map, col=changer)
 
-total_monomoy <- 3676+2012+4924+5045+115923
+total_monomoy <- 
 total_monomoy
 # [1] 131580
 
-freq(classmonomoy_2014_4$map)
+freq(classmonomoy_2014_3$map)
 #      value  count
-# [1,]     1   3676 (dune vegetation)
-# [2,]     2   2012 (salt marsh)
-# [3,]     3   4924 (tidal sand bars)
-# [4,]     4   5045 (bare sand)
-# [5,]    NA 115923 (water)
 
-prop_monomoy_2014 <- freq(classmonomoy_2014_4$map)/total_monomoy
+
+prop_monomoy_2014 <- freq(classmonomoy_2014_3$map)/total_monomoy
 prop_monomoy_2014
 #      count (2014)
-# 0.02793738 (dune vegetation)
-# 0.01529108 (salt marsh)
-# 0.03742210 (tidal sand bars)
-# 0.03834169 (bare sand)
-# 0.88100775 (water)
 
-freq(classmonomoy_2016_4$map)
+
+freq(classmonomoy_2016_3$map)
 #      value  count
-# [1,]     1   2173 (salt marsh)
-# [2,]     2   5865 (tidal sand bars)
-# [3,]     3   6238 (dune vegetation)
-# [4,]     4   3975 (bare sand)
-# [5,]    NA 113329 (water)
 
-prop_monomoy_2016 <- freq(classmonomoy_2016_4$map)/total_monomoy
+
+prop_monomoy_2016 <- freq(classmonomoy_2016_3$map)/total_monomoy
 prop_monomoy_2016
 #      count (2016)
-# 0.01651467 (salt marsh)
-# 0.04457364 (tidal sand bars)
-# 0.04740842 (dune vegetation)
-# 0.03020976 (bare sand)
-# 0.86129351 (water)
 
-freq(classmonomoy_2018_4$map)
+
+freq(classmonomoy_2018_3$map)
 #      value  count
-# [1,]     1   2605 (salt marsh)
-# [2,]     2   3100 (dune vegetation)
-# [3,]     3   3111 (tidal sand bars)
-# [4,]     4   5570 (bare sand)
-# [5,]    NA 117194 (water)
 
-prop_monomoy_2018 <- freq(classmonomoy_2018_4$map)/total_monomoy
+
+
+prop_monomoy_2018 <- freq(classmonomoy_2018_3$map)/total_monomoy
 prop_monomoy_2018
 #      count (2018)
-# 0.01979784 (salt marsh)
-# 0.02355981 (dune vegetation)
-# 0.02364341 (tidal sand bars)
-# 0.04233166 (bare sand)
-# 0.89066727 (water)
 
-freq(classmonomoy_2020_4$map)
+
+freq(classmonomoy_2020_3$map)
 #      value  count
-# [1,]     1   1389 (salt marsh)
-# [2,]     2   4024 (bare sand)
-# [3,]     3   1207 (tidal sand bars)
-# [4,]     4   3313 (dune vegetation)
-# [5,]    NA 121647 (water)
 
-prop_monomoy_2020 <- freq(classmonomoy_2020_4$map)/total_monomoy
+
+prop_monomoy_2020 <- freq(classmonomoy_2020_3$map)/total_monomoy
 prop_monomoy_2020
 #      count (2020)
-# 0.010556316 (salt marsh)
-# 0.030582155 (bare sand)
-# 0.009173127 (tidal sand bars)
-# 0.025178599 (dune vegetation)
-# 0.924509804 (water)
+
 
 # __________Monomoy data frame________________________________________________________________________________________________________________________________________________
 
